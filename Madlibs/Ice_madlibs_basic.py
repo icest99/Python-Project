91a1de592f3c45a7b261e7f32c1ace493b97ab31
icest99 <playicest@gmail.com>
2020-12-22 20:07:38 +0700

    Initial commit

---
+++ Madlibs/Ice_madlibs_basic.py
@@ -0,0 +1,6 @@
+noun1 = input("Enter noun1: ")
+noun2 = input("Enter noun2: ")
+noun3 = input("Enter noun3: ")
+verb = input("Enter noun4: ")
+print ("rumble in the " + noun1 + ", Dancing in the " + noun2 + ", Writing the " + noun3 + " Untill it all" + verb)
+
